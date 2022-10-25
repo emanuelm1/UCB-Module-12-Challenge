de7ad6497d1fd4865c364f2e49465667d1848179
emanuelm1 <emanuelm52001@gmail.com>
2022-10-25 13:04:01 -0700

    Update db.sql

--- db/db.sql
+++ db/db.sql
@@ -3,9 +3,8 @@ CREATE DATABASE business;
 USE business;
 
 CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(20)NOT NULL,
     last_name VARCHAR(20) NOT NULL
-    PRIMARY KEY (id)
 
 );
